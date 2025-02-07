781be6f789c70e0897e095d03667a55016a32c29
GeremyOcsa <geremy.ocsa11@gmail.com>
2025-02-06 21:54:22 -0500

    cors update

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.listen(app.get('port'), () => {
 })
 
 app.use(cors({
-  origin: ['http://127.0.0.1:5500']
+  origin: ['http://127.0.0.1:5500', 'https://sistema.clinica-flores.com/']
 }))
 
 app.get('/registros', async (req, res) => {
